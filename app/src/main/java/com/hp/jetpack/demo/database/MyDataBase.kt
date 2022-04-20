cf9d69175a5eb03321445f6f3d1c13fc5f3df692
hp8952 <hp8952@126.com>
2022-04-20 16:44:42 +0800

    1.桌面图标快捷启动

--- app/src/main/java/com/hp/jetpack/demo/database/MyDataBase.kt
+++ app/src/main/java/com/hp/jetpack/demo/database/MyDataBase.kt
@@ -6,7 +6,7 @@ import com.hp.jetpack.demo.database.dao.UserDao
 import com.hp.jetpack.demo.database.entity.User
 
 // 修改版本号，非常麻烦
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class MyDataBase : RoomDatabase() {
     companion object {
         const val DATA_BASE_NAME = "db_test";
